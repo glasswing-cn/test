9b611566943459570a159440d201a596f3ed8b33
glasswing <glasswing@sina.com>
2019-03-08 19:13:05 +0800

    add Miller-Rabin primality test to prime.py

--- prime.py
+++ prime.py
@@ -126,9 +126,76 @@ def is_primeF(n) :
         return False
     elif n==2 or n==3 or n==5 :
         return True
-    elif n<5 :
+    elif (n&1)==0 :
         return False
     return powermod(2, n-1, n)==1 and powermod(3, n-1, n)==1 and powermod(5, n-1, n)==1
+
+def ctz(n) :
+    """所谓Count Trailing Zeros指令，返回整数n末尾0的个数（最低位1的位置）
+
+    >>> ctz(0)
+    0
+    >>> ctz(1)
+    0
+    >>> ctz(40)
+    3
+    >>> ctz(1024)
+    10
+    """
+    return (n^(n-1)).bit_length() - 1
+
+def MR_test(a, n) :
+    """Miller Rabin素性测试，改进的费马素性测试 a^(p-1)==1 (mod p)。
+       因(p-1)为偶数，令(p-1)=(2^s)*d，依次计算平方序列x = a^d,a^2d,a^4d...a^((2^s)*d) (mod p)，找到1即可知p通过费马测试。
+       基于x^2==1 (mod p)只存在平凡的平方根（x==1、-1），进一步对p筛选：发现1(mod p)非平凡的平方根，则未通过Miller-Rabin测试。
+       返回值: 未通过费马测试返回0，a^d==1或-1返回1，发现x^2==-1(mod p)返回x==SQRT(-1)，发现x^2==1(mod p)返回-x==-SQRT(1)。
+
+    >>> MR_test(2, 341)
+    -32
+    >>> MR_test(3, 341)
+    0
+    >>> MR_test(5, 341)
+    0
+    >>> MR_test(2, 1729)
+    -1065
+    >>> MR_test(3, 1729)
+    -664
+    >>> MR_test(5, 1729)
+    -1065
+    >>> MR_test(2, 25326001)
+    1
+    >>> MR_test(3, 25326001)
+    1
+    >>> MR_test(5, 25326001)
+    1
+    >>> MR_test(7, 25326001)
+    0
+    """
+    x = powermod(a, n>>ctz(n-1), n)
+    if x==1 or x==n-1 :
+        return 1
+    for i in range(1, ctz(n-1)) :
+        y = x*x%n
+        if y==n-1 :
+            return x
+        elif y==1 :
+            return -x
+        x = y
+    else :
+        y = x*x%n
+        if y==1 :
+            return -x
+        else :
+            return 0
+
+def is_primeM(n) :
+    if n<2 :
+        return False
+    elif n==2 or n==3 or n==5 :
+        return True
+    elif (n&1)==0 :
+        return False
+    return MR_test(2, n)>0 and MR_test(3, n)>0 and MR_test(5, n)>0
 
 def repunit_prime(s, n) :
     """因为循环节不是1和循环次数非素数的循环整数一定是合数，这个函数寻找循环次数少于n的形如001001...这样的素数。
